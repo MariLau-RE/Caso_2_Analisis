1d8d823b813fc9d59839c279cf90c61bc8184209
MariLau-RE <marilaure29@estudiantec.cr>
2022-03-23 09:28:18 -0600

    actualizacion

--- Caso_2_Maria_Laura_Retana.cpp
+++ Caso_2_Maria_Laura_Retana.cpp
@@ -15,34 +15,36 @@ long int acum_ticks_for= 0;
 long int acum_ticks_while= 0;
 
 ticks_for = GetTickCount();
-cout <<"GetTickCount for: "<<ticks_for<<endl;
+cout<<endl;
+cout << "Largo de lista: "<< largo <<endl;
+cout <<"GetTickCount inicial: "<<ticks_for<<endl;
 
 for (i = 1; i < largo; i++){
 /* indice j explora la sublista a[i-1]..a[0] buscando la
 posicion correcta del elemento destino, lo asigna a a[j] */
-cout<< "In for"<<endl;
+//cout<< "In for"<<endl;
 j = i;
 aux = lista[i];
 /* se localiza el punto de inserción explorando hacia abajo */
 
 ticks_while = GetTickCount();
-cout <<"GetTickCount while: "<<ticks_while<<endl;
+//cout <<"GetTickCount while: "<<ticks_while<<endl;
 
 while (j > 0 && aux < lista[j-1]) {
 /* desplazar elementos hacia arriba para hacer espacio */
-cout<< "In while"<<endl;
+//cout<< "In while"<<endl;
 lista[j] = lista[j-1];
 j--;
 }
 ticks_while = GetTickCount() - ticks_while;
 acum_ticks_while = acum_ticks_while + ticks_while;
-cout << "While took: " << ticks_while << " ticks" << endl;
+//cout << "While took: " << ticks_while << " ticks" << endl;
 
 lista[j] = aux;
 }
 ticks_for = GetTickCount() - ticks_for;
 acum_ticks_for = acum_ticks_for + ticks_for;
-
+    cout <<"GetTickCount final: "<<GetTickCount()<<endl;
 	cout << "While took a total of " << acum_ticks_while << " ticks." << endl;
 	cout << "For took a total of " << acum_ticks_for << " ticks" << endl;
 	
@@ -87,15 +89,23 @@ int main(){
 	
     	cout << lista[i] << " ";
    	}*/
-    int lista2 []={21,6,9,3,4,1,19,5,7,1,2};
+    int i;
+    int j;
+    int cantitems;
+    for ( i=0; i<10; i++){
+        cantitems = 5000*(i+1);
+        int lista2 [cantitems];
+        for ( j = 0; j < cantitems; j++){
+           lista2[j]=cantitems-j;
+        }  
+    //int lista2 []={21,6,9,3,4,1,19,5,7,1,2};
     int largo2 = sizeof(lista2) / sizeof(lista2[0]);
     Insercion(lista2, largo2);
-    cout << "\nLista ordenada: ";
-    for (int i = 0; i < largo2; i++){
-	
-    	cout << lista2[i] << " ";
-   	}
- 
+    //cout << "\nLista ordenada: ";
+    //for (int i = 0; i < largo2; i++){
+	//    	cout << lista2[i] << " ";
+   	//}
+    }
 
     return 0;
 
